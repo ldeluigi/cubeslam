eb3db9204f44a303217922d98acabba77887c5ab
ldeluigi <lucadelu97@gmail.com>
2020-01-30 17:32:47 +0100

    Fixed cookie module

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,9 @@ module.exports = {
   entry: "./index.js",
   output: {
     filename: "build.js",
-    path: path.resolve(__dirname, "build")
+    path: path.resolve(__dirname, "build"),
+    library: 'slam',
+    libraryTarget: 'global'
   },
   target: "web",
   optimization: {
@@ -31,7 +33,8 @@ module.exports = {
   ],
   resolve: {
     alias: {
-      jquery: path.resolve(__dirname, "./jquery/jquery.js")
+      jquery: path.resolve(__dirname, "./jquery/jquery.js"),
+      cookie: "component-cookie"
     }
   },
   node: { fs: "empty", tls: "empty" }
